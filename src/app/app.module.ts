8c8f56113e0e77131b1ee3ad890f45d3bb8828b9
Anton V <twojuly89@gmail.com>
2020-07-31 00:02:00 +0700

    PWA works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '@shared/shared.module';
 import { PagesModule } from './modules/pages/pages.module';
 import { CoreModule } from '@core/core.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +19,7 @@ import { CoreModule } from '@core/core.module';
     SharedModule,
     CoreModule,
     PagesModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent],

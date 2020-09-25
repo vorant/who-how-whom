2bd911bb8dd53376afd006ad6c8a0f691ab5bc82
Anton V <twojuly89@gmail.com>
2020-09-25 13:16:50 +0700

    implement cypress

---
+++ cypress/integration/whw/first.spec.ts
@@ -0,0 +1,9 @@
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.not.equal(false);
+  });
+
+  it('Does much!', () => {
+    expect(true).to.equal(true);
+  });
+});

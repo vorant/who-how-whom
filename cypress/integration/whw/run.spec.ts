0eee706fa97e4f121e7f1cf1b6b4b92849d9d4d2
Anton V <twojuly89@gmail.com>
2020-09-27 00:35:52 +0700

    add e2e

---
+++ cypress/integration/whw/run.spec.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('body should contain .mat-typography class', () => {
+    cy.get('body').should('have.class', 'mat-typography');
+  });
+});
